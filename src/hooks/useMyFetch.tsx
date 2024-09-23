322eef48199479e5eb3bf78c356b2932abafc829
afolabi-ola <afolabiquadri28@gmail.com>
2024-09-23 13:11:39 +0100

    adjusted external link2

--- src/hooks/useMyFetch.tsx
+++ src/hooks/useMyFetch.tsx
@@ -1,20 +1,64 @@
 import { useEffect, useState } from 'react';
 
-function useMyEffect() {
-  const [myImages, setMyImages] = useState<
-    { name: string; src: string; desc: string }[]
-  >([]);
 
-  useEffect(function () {
-    async function data() {
-      const res = await fetch('http://localhost:3000/images');
-      const data = await res.json();
-      setMyImages([...data]);
-    }
-    data();
-  }, []);
+  const images = [
+    {
+      name: 'Image 1',
+      src: './images/img1.jpg',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsa libero pariatur. Fugit sunt adipisci similique odio beatae nobis illum minima impedit expedita. Autem est, iste voluptatem fugit odit adipisci!',
+    },
+    {
+      name: 'Image 2',
+      src: 'images/img2.png',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsa libero pariatur. Fugit sunt adipisci similique odio beatae nobis illum minima impedit expedita. Autem est, iste voluptatem fugit odit adipisci!',
+    },
+    {
+      name: 'Image 3',
+      src: 'images/img3.jpg',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsa libero pariatur. Fugit sunt adipisci similique odio beatae nobis illum minima impedit expedita. Autem est, iste voluptatem fugit odit adipisci!',
+    },
+    {
+      name: 'Image 4',
+      src: 'images/img4.jpg',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsa libero pariatur. Fugit sunt adipisci similique odio beatae nobis illum minima impedit expedita. Autem est, iste voluptatem fugit odit adipisci!',
+    },
+    {
+      name: 'Image 5',
+      src: 'images/img5.jpg',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsa libero pariatur. Fugit sunt adipisci similique odio beatae nobis illum minima impedit expedita. Autem est, iste voluptatem fugit odit adipisci!',
+    },
+    {
+      name: 'Image 6',
+      src: 'images/img6.jpg',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsa libero pariatur. Fugit sunt adipisci similique odio beatae nobis illum minima impedit expedita. Autem est, iste voluptatem fugit odit adipisci!',
+    },
+  ];
 
-  return { myImages };
-}
+  function useMyEffect() {
+    const [myImages, setMyImages] = useState<
+      { name: string; src: string; desc: string }[]
+    >([]);
+
+    useEffect(function () {
+      async function data() {
+        try {
+          const res = await fetch('http://localhost:3000/images');
+          console.log(res);
+
+          if (!res.ok) {
+            throw new Error('could not get images');
+          }
+          const data = await res.json();
+          setMyImages([...data]);
+        } catch (error) {
+          console.log(error);
+          setMyImages([...images]);
+        }
+      }
+      data();
+    }, []);
+
+    return { myImages };
+  }
 
 export default useMyEffect;
